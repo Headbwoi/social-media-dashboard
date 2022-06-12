81352ef5fcdef1452fa87edf58d1ac769a9ca69b
headbwoi <tochukwujohn24@gmail.com>
2022-06-12 20:54:03 +0100

    finished project setup

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true
+    },
+    fontFamily: {
+      inter: ['"Inter", "sans-serif"']
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1115px",
+    },
+    colors: {
+      limeGreen: "hsl(163, 72%, 41%)",
+      brightRed: "hsl(356, 69%, 56%)",
+      Facebook: "hsl(208, 92%, 53%)",
+      Twitter: "hsl(203, 89%, 53%)",
+      instagramGradientOne: "hsl(37, 97%, 70%)",
+      instagramGradientTwo: "hsl(329, 70%, 58%)",
+      YouTube: "hsl(348, 97%, 39%)",
+      background: "var(--BG)",
+      topBgPattern: "var(--topBGPattern)",
+      cardBg: "var(--cardBG)",
+      blueText: "var(--bluetext)",
+      Text: "var(--Text)",
+      toggleBg: "var(--toggleBg)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
